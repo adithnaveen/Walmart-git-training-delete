1b7ba9e0d0715e89891ef69ca88407732c33b5af
Atul Kumar - a0k02o0 <Atul.Kumar@walmartlabs.com>
2024-08-30 12:20:29 +0530

    adding Accounts

---
+++ Accounts.java
@@ -0,0 +1,20 @@
+public class Accounts {
+    private int accountId;
+    private int accountName;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(int accountName) {
+        this.accountName = accountName;
+    }
+}
