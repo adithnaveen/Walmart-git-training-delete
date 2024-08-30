b87ce139eb826d6f1c60654260075b546aa9a90c
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2024-08-30 11:03:42 +0530

    created add function in hello world file

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public static void main(String [] args) {
 		System.out.println("Hello");
 	}
+
+
+	public static int add(int num1, int num2 ) {
+		return num1 + num2 ; 
+	}
 }
 
 
